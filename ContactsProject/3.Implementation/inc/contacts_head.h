1d6a5ca06fdaef8fda8f7c076feb6a3af9a1d8a5
99002609 <71481394+99002609@users.noreply.github.com>
2020-09-19 11:37:33 +0530

    Update contacts_head.h

--- ContactsProject/3.Implementation/inc/contacts_head.h
+++ ContactsProject/3.Implementation/inc/contacts_head.h
@@ -1,1 +1,12 @@
 
+#ifndef __Contact_Management_H__
+#define __Contact_Management_H__
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<process.h>
+#include<stdlib.h>
+#include<dos.h>
+
+#endif
